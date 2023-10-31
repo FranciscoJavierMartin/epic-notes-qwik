34f12c78d67dc75903415bd3508e9ae8ccae5a3b
Francisco <javiermartincuenca@gmail.com>
2023-10-31 22:22:08 +0100

    Style form

--- src/routes/users/[username]/notes/[noteId]/edit/index.tsx
+++ src/routes/users/[username]/notes/[noteId]/edit/index.tsx
@@ -1,8 +1,10 @@
-import Input from '@/components/ui/input';
-import Label from '@/components/ui/label';
-import { kodyNotes } from '@/db/db.server';
 import { component$ } from '@builder.io/qwik';
 import { Form, routeLoader$ } from '@builder.io/qwik-city';
+import Button from '@/components/ui/button';
+import Input from '@/components/ui/input';
+import Label from '@/components/ui/label';
+import Textarea from '@/components/ui/textarea';
+import { kodyNotes } from '@/db/db.server';
 
 export const useNote = routeLoader$(async ({ params, error }) => {
 	const note = kodyNotes.find((note) => note.id === params.noteId);
@@ -16,13 +18,24 @@ export const useNote = routeLoader$(async ({ params, error }) => {
 
 export default component$(() => {
 	const data = useNote();
+
 	return (
-		<Form>
-			<div>
+		<Form class='flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12'>
+			<div class='flex flex-col gap-1'>
 				<div>
 					<Label>Title</Label>
 					<Input name='title' value={data.value.note.title} />
 				</div>
+				<div>
+					<Label>Content</Label>
+					<Textarea name='content' value={data.value.note.content} />
+				</div>
+			</div>
+			<div class='floating-toolbar'>
+				<Button variant='destructive' type='reset'>
+					Reset
+				</Button>
+				<Button type='submit'>Submit</Button>
 			</div>
 		</Form>
 	);

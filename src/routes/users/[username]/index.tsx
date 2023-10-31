44df5023b7c8d35dc5ef057faceb4565e4ec5b37
Francisco <javiermartincuenca@gmail.com>
2023-10-31 20:55:03 +0100

    User profile page

--- src/routes/users/[username]/index.tsx
+++ src/routes/users/[username]/index.tsx
@@ -1,13 +1,30 @@
 import { component$ } from '@builder.io/qwik';
-import { Link, useLocation } from '@builder.io/qwik-city';
+import { Link, routeLoader$, useLocation } from '@builder.io/qwik-city';
+
+export const useUserProfile = routeLoader$(async ({ params }) => {
+	const user = {
+		id: '9d6eba59daa2fc2078cf8205cd451041',
+		email: 'kody@kcd.dev',
+		username: 'kody',
+		name: 'Kody',
+		createdAt: new Date('2023-10-30T22:27:04.762Z'),
+	};
+
+	return { user };
+});
 
 export default component$(() => {
 	const location = useLocation();
+	const data = useUserProfile();
 
 	return (
-		<>
-			<h1>Hello {location.params.username}</h1>
-			<Link href={`/users/${location.params.username}/notes`}>Notes</Link>
-		</>
+		<div class='container mb-48 mt-36'>
+			<h1 class='text-h1'>
+				{data.value.user.name ?? data.value.user.username}
+			</h1>
+			<Link href={`/users/${location.params.username}/notes`} class='underline'>
+				Notes
+			</Link>
+		</div>
 	);
 });

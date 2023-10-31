3bffb96b55409a530d726eb3cfc56d0adb692448
Francisco <javiermartincuenca@gmail.com>
2023-10-31 20:47:39 +0100

    Main layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,28 @@
 import { component$, Slot } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
-	return <Slot />;
+	return (
+		<>
+			<header class='container mx-auto py-6'>
+				<nav class='flex justify-between'>
+					<Link href='/'>
+						<div class='font-light'>epic</div>
+						<div class='font-bold'>notes</div>
+					</Link>
+				</nav>
+			</header>
+			<div class='flex-1'>
+				<Slot />
+			</div>
+			<div class='container mx-auto flex justify-between'>
+				<Link href='/'>
+					<div class='font-light'>epic</div>
+					<div class='font-bold'>notes</div>
+				</Link>
+				<p>Built with ♥️ by John Doe</p>
+			</div>
+			<dic class='h-5' />
+		</>
+	);
 });

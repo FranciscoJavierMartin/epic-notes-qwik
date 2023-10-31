3af755994534e812ae5bc9c5dcc809e112bd655d
Francisco <javiermartincuenca@gmail.com>
2023-10-31 19:44:59 +0100

    Update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import animatePlugin from 'tailwindcss-animate';
 
-module.exports = {
+export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	darkMode: 'class',
 	theme: {

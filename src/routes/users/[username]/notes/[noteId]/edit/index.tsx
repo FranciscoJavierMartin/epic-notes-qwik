ec5a02e053f80e7fa48e4b57e7689e125315f384
Francisco <javiermartincuenca@gmail.com>
2023-10-31 22:16:32 +0100

    Get note

--- src/routes/users/[username]/notes/[noteId]/edit/index.tsx
+++ src/routes/users/[username]/notes/[noteId]/edit/index.tsx
@@ -1,5 +1,29 @@
+import Input from '@/components/ui/input';
+import Label from '@/components/ui/label';
+import { kodyNotes } from '@/db/db.server';
 import { component$ } from '@builder.io/qwik';
+import { Form, routeLoader$ } from '@builder.io/qwik-city';
+
+export const useNote = routeLoader$(async ({ params, error }) => {
+	const note = kodyNotes.find((note) => note.id === params.noteId);
+
+	if (!note) {
+		throw error(404, 'Note not found');
+	}
+
+	return { note: { title: note.title, content: note.content } };
+});
 
 export default component$(() => {
-	return <h1>Edit</h1>;
+	const data = useNote();
+	return (
+		<Form>
+			<div>
+				<div>
+					<Label>Title</Label>
+					<Input name='title' value={data.value.note.title} />
+				</div>
+			</div>
+		</Form>
+	);
 });

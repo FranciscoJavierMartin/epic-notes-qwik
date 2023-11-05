8cfcf4e087796b9df92255f85a3134a75f978b60
Francisco <javiermartincuenca@gmail.com>
2023-11-05 19:25:45 +0100

    Companion

--- src/components/ui/status-button.tsx
+++ src/components/ui/status-button.tsx
@@ -1,17 +1,31 @@
 import { Slot, component$ } from '@builder.io/qwik';
 import Button, { type ButtonProps } from '@/components/ui/button';
 import { cn } from '@/utils/misc';
+import { Icon } from './icon';
 
 interface StatusButtonProps extends ButtonProps {
 	status: 'pending' | 'success' | 'error' | 'idle';
+	message?: string | null;
 }
 
 export default component$<StatusButtonProps>(
 	({ status, class: className, ...props }) => {
 		const companion = {
-			pending: <span class='inline-block animate-spin'>🌀</span>,
-			success: <span>✅</span>,
-			error: <span>❌</span>,
+			pending: true ? (
+				<div class='inline-flex h-6 w-6 items-center justify-center'>
+					<Icon name='update' class='animate-spin' />
+				</div>
+			) : null,
+			success: (
+				<div class='inline-flex h-6 w-6 items-center justify-center'>
+					<Icon name='check' />
+				</div>
+			),
+			error: (
+				<div class='inline-flex h-6 w-6 items-center justify-center rounded-full bg-destructive'>
+					<Icon name='cross-1' class='text-destructive-foreground' />
+				</div>
+			),
 			idle: null,
 		}[status];
 

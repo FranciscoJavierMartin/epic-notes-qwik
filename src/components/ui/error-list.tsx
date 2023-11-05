78167cc634ed69efca1cdceb7fb335700ecce221
Francisco <javiermartincuenca@gmail.com>
2023-11-05 20:34:13 +0100

    Error list

--- src/components/ui/error-list.tsx
+++ src/components/ui/error-list.tsx
@@ -1,15 +1,19 @@
 import { component$ } from '@builder.io/qwik';
 
+type ListOfErrors = Array<string | null | undefined> | null | undefined;
+
 interface ErrorListProps {
 	id?: string;
-	errors?: string[] | null;
+	errors?: ListOfErrors;
 }
 
 export default component$<ErrorListProps>(({ id, errors }) => {
-	return errors?.length ? (
+	const errorsToRender = errors?.filter(Boolean);
+
+	return errorsToRender?.length ? (
 		<ul id={id} class='flex flex-col gap-1'>
-			{errors.map((error, i) => (
-				<li key={i} class='text-[10px] text-foreground-destructive'>
+			{errorsToRender.map((error) => (
+				<li key={error} class='text-[10px] text-foreground-destructive'>
 					{error}
 				</li>
 			))}

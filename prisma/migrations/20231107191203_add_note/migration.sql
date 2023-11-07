bf3c1148de97ee1579fbe666c77f6b06aecc294f
Francisco <javiermartincuenca@gmail.com>
2023-11-07 20:27:23 +0100

    First version of seed

---
+++ prisma/migrations/20231107191203_add_note/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    CONSTRAINT "Note_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "NoteImage" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "altText" TEXT,
+    "contentType" TEXT NOT NULL,
+    "blob" BLOB NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "noteId" TEXT NOT NULL,
+    CONSTRAINT "NoteImage_noteId_fkey" FOREIGN KEY ("noteId") REFERENCES "Note" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "UserImage" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "altText" TEXT,
+    "contentType" TEXT NOT NULL,
+    "blob" BLOB NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "UserImage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserImage_userId_key" ON "UserImage"("userId");
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,33 +3,47 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-prisma.note.findFirst().then((firstNote) => {
-	if (!firstNote) {
-		throw new Error('You need to have a note in the database first');
-	}
-
-	return Promise.all([
-		fs.promises.readFile('./tests/fixtures/images/kody-notes/cute-koala.png'),
-		fs.promises.readFile('./tests/fixtures/images/kody-notes/koala-eating.png'),
-	]).then(([image1, image2]) => {
-		return prisma.note.update({
-			where: { id: firstNote.id },
+prisma.user.deleteMany().then(() => {
+	return prisma.user
+		.create({
 			data: {
-				images: {
-					create: [
-						{
-							altText: 'an adorable koala cartoon illustration',
-							contentType: 'image/png',
-							blob: image1,
-						},
-						{
-							altText: 'a cartoon illustration of a koala in a tree eating',
-							contentType: 'image/png',
-							blob: image2,
-						},
-					],
-				},
+				email: 'kody@kcd.dev',
+				username: 'kody',
+				name: 'Kody',
 			},
+		})
+		.then((kody) => {
+			return Promise.all([
+				fs.promises.readFile(
+					'./tests/fixtures/images/kody-notes/cute-koala.png',
+				),
+				fs.promises.readFile(
+					'./tests/fixtures/images/kody-notes/koala-eating.png',
+				),
+			]).then(([image1, image2]) => {
+				return prisma.note.create({
+					data: {
+						id: 'd27a197e',
+						title: 'Basic Koala Facts',
+						content:
+							'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
+						ownerId: kody.id,
+						images: {
+							create: [
+								{
+									altText: 'an adorable koala cartoon illustration',
+									contentType: 'image/png',
+									blob: image1,
+								},
+								{
+									altText: 'a cartoon illustration of a koala in a tree eating',
+									contentType: 'image/png',
+									blob: image2,
+								},
+							],
+						},
+					},
+				});
+			});
 		});
-	});
 });
